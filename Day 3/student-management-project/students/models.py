f557c1b918be64675e23be80932cb828592562d0
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-09-20 01:42:38 +0530

    all files of models and orm

---
+++ Day 3/student-management-project/students/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField()
+    course = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
---
+++ Day 3/student-management-project/students/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Student
+
+# Create your views here.
+
+
+
+def student_list(request):
+    students = Student.objects.all()
+
+    return render(
+        request,
+        "students/student_list.html",
+        {"students": students}
+    )
